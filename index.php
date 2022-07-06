a3481ae9efca67155b98a3e6576155d920975c4a
OgyDotMy <76845848+OgyDotMy@users.noreply.github.com>
2022-07-07 02:33:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Demo php Debug</title>
+</head>
+<body>
+    <form action="logic.php" method="post">
+        <label for="">Nama</label>
+        <input type="text" name="nama" id="">
+        <label for="">Alamat</label>
+        <input type="text" name="alamat" id="">
+        <button type="submit" name="submit">submit data</button>
+    </form>
+    
+    
+
+</body>
+</html>
