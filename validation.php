735c8fc1cc1cdc968cb9713dc7b8c4f461808339
OgyDotMy <76845848+OgyDotMy@users.noreply.github.com>
2022-07-07 02:34:37 +0800

    Create validation.php

---
+++ validation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="core/valid.php" method="post">
+        <label for="nama">Nama</label>
+        <input type="text" name="nama" id="" placeholder="input your name"> <?php ?><br />
+        <label for="password">password</label>
+        <input type="password" name="password" id="" placeholder="input your password"> <br>
+        <label for="emel">emel</label>
+        <input type="email" name="emel" id="" placeholder="input your emel"> <br>
+        <input type="submit" value="submit" name="inputdata">
+    </form>
+
+    
+    
+</body>
+</html>
+
